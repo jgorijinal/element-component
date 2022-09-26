13eba61a710a9c90157b3cf6871ea2770706bd35
jgoriginal <932274204@qq.com>
2022-09-26 20:25:50 +0800

    趋势组件的的自定义图标,自定义颜色

--- src/components/trend/src/index.vue
+++ src/components/trend/src/index.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="container">
     <div class="trend">
-      <div class="text">{{ text }}</div>
+      <div class="text" :style="{ color: textColor }">
+        <slot v-if="slots.default"></slot>
+        <div v-else>{{ text }}</div>
+      </div>
       <div class="icon">
-        <el-icon-arrow-up
+        <!-- <el-icon-arrow-up
           v-if="type === 'up'"
           :style="{ color: upIconColor }"
         />
-        <el-icon-arrow-down v-else :style="{ color: downIconColor }" />
+        <el-icon-arrow-down v-else :style="{ color: downIconColor }" /> -->
+        <component
+          :is="`el-icon${toLine(upIcon)}`"
+          v-if="type === 'up'"
+          :style="{ color: upIconColor }"
+        ></component>
+        <component
+          :is="`el-icon${toLine(downIcon)}`"
+          v-else
+          :style="{ color: downIconColor }"
+        ></component>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useSlots, computed } from "vue";
+import { toLine } from "../../../utils";
 const props = defineProps({
   // 趋势类型
   type: {
@@ -32,6 +47,29 @@ const props = defineProps({
     type: String,
     default: "#52c41a",
   },
+  upTextColor: {
+    type: String,
+    default: "black",
+  },
+  downTextColor: {
+    type: String,
+    default: "black",
+  },
+  upIcon: {
+    type: String,
+    default: "ArrowUp",
+  },
+  downIcon: {
+    type: String,
+    default: "ArrowDown",
+  },
+});
+// 对插槽的处理
+const slots = useSlots();
+
+// 计算文字颜色
+const textColor = computed(() => {
+  return props.type === "up" ? props.upTextColor : props.downTextColor;
 });
 </script>
 
--- src/views/trend/index.vue
+++ src/views/trend/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <trend text="上升趋势"></trend>
-  <trend text="下降趋势" type="down"></trend>
+  <trend text="上升趋势">插槽的值</trend>
+  <trend text="下降趋势" type="down" downTextColor="yellow"></trend>
 </template>
 <script setup lang="ts"></script>
 
